3583ac6e2666e88364a4562e7b29d4bb56a77b15
SadChimera <jennifer.lee24@student.cart.org>
2025-05-06 20:57:56 -0700

    Disonysus

--- lily/lily.js
+++ lily/lily.js
@@ -6,7 +6,12 @@ const lines = [
   "The Pope looks down at you, seemingly surprised to find an eavesdropper.",
   "\"What do you think, sire? Execute the vermin?\"",
   "The Pope eerily hums, watching you, deciding your worth.",
-  "\"Keep this young believer alive.\""
+  "\"Keep this young believer alive.\"",
+ "\"Why? Why should we bother?.\"",
+ "\"Because, daughter Lily...\"",
+ "You felt the Pope's eyes crawl over to you.",
+ "\"One shall pay for the sin of eavesdropping on a precious sight.\"",
+ "\"Why? Why should we bother?.\"",
 ];
 
 let currentLine = 0;
